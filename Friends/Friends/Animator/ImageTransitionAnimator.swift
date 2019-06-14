3241eb0cdeeb9df0cf9ac5c24564ef14c7d0507c
Stephanie Bowles <sbowl001@fiu.edu>
2019-06-14 12:54:14 +0800

    almost done?

--- Friends/Friends/Animator/ImageTransitionAnimator.swift
+++ Friends/Friends/Animator/ImageTransitionAnimator.swift
@@ -11,11 +11,85 @@ import UIKit
 class ImageTransitionAnimator: NSObject, UIViewControllerAnimatedTransitioning {
    
 //    Add properties for the views that the animator will need to know about. These are the source and destination photo views, and the source and destination name labels.
-    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        
-    }
+    var sourceImageView: UIImageView?
+    var destinationImageView: UIImageView?
+    var sourceNameLabel: UILabel?
+    var destinationNameLabel: UILabel?
+    
     
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
-        return 0.5
+        return 1.0
     }
+    
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        guard let fromVC = transitionContext.viewController(forKey: .from),
+              let toVC = transitionContext.viewController(forKey: .to),
+              let toView = transitionContext.view(forKey: .to) else {return}
+        
+       //        Create a temporary image view and label to be used for the animation and add them to the transitionContext's containerView.
+        guard let toImage = destinationImageView,
+              let toLabel = destinationNameLabel,
+              let fromImage = sourceImageView,
+            let fromLabel = sourceNameLabel  else {return}
+        
+//        Get the start and end frames for the image view and label.
+         let containerView = transitionContext.containerView
+        
+        let toViewEndFrame = transitionContext.finalFrame(for: toVC)
+        
+        containerView.addSubview(toView)
+        toView.frame = toViewEndFrame
+        
+        
+        //        Set up any properties that should be set up before beginning the animation. For example, hide the destination views by setting their alpha to 0.0.
+        toView.alpha = 0
+        
+        toLabel.alpha = 0
+        fromLabel.alpha = 0
+        
+        toImage.alpha = 0
+        fromImage.alpha = 0
+        
+      
+        let transitionLabelInitialFrame = containerView.convert(fromLabel.bounds, from: fromLabel)
+        let transitionImageInitialFrame = containerView.convert(fromImage.bounds, from: toImage)
+        
+        
+        let transitioningLabel = UILabel(frame: transitionLabelInitialFrame)
+        transitioningLabel.textColor = .black
+        transitioningLabel.font = fromLabel.font
+        transitioningLabel.text = fromLabel.text
+        containerView.addSubview(transitioningLabel)
+        
+        let transitioningImage = UIImageView(frame: transitionImageInitialFrame)
+        transitioningImage.image = fromImage.image
+        containerView.addSubview(transitioningImage)
+        
+        let transitionDuration = self.transitionDuration(using: transitionContext)
+        
+        toView.layoutIfNeeded()
+        
+//        Use UIView.animate() to animate the transition.
+        
+        UIView.animate(withDuration: transitionDuration, animations: {
+            let transitioningLabelEndFrame = containerView.convert(toLabel.bounds, from: toLabel)
+            transitioningLabel.frame = transitioningLabelEndFrame
+            let transitioningImageEndFrame = containerView.convert(toImage.bounds, from: toImage)
+            transitioningImage.frame = transitioningImageEndFrame
+            toView.alpha = 1
+        }) { (_) in
+            toLabel.alpha = 1
+            fromLabel.alpha = 1
+            toImage.alpha = 1
+            fromImage.alpha = 1
+            transitioningLabel.removeFromSuperview()
+            transitioningImage.removeFromSuperview()
+            transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
+        }
+//        In the animation's completion closure, (re)set the correct properties for the destination views, and remove the temporary animation views. Call transitionContext.completeTransition().
+
+    }
+    
 }
--- Friends/Friends/Animator/NavigationControllerDelegate.swift
+++ Friends/Friends/Animator/NavigationControllerDelegate.swift
@@ -12,9 +12,35 @@ import UIKit
 class NavigationControllerDelegate: NSObject, UINavigationControllerDelegate {
     
     var sourceCell: UITableViewCell?
-//    func navigationController(_ navigationController:, animationControllerFor:, from:, to:) {
+    let animator = ImageTransitionAnimator()
     
-//    }
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        
+        toVC.loadViewIfNeeded()
+        
+        if let toVC = toVC  as? FriendsDetailViewController,
+            let sourceCell = sourceCell {
+            
+            animator.sourceImageView  = sourceCell.imageView
+            animator.destinationImageView = toVC.pictureView
+            
+            animator.sourceNameLabel = sourceCell.textLabel
+            animator.destinationNameLabel = toVC.nameLabel
+            
+        }  else if let fromVC = fromVC as? FriendsDetailViewController {
+            
+            animator.destinationImageView = sourceCell?.imageView
+            animator.sourceImageView = fromVC.pictureView
+            
+            animator.destinationNameLabel = sourceCell?.textLabel
+            animator.sourceNameLabel = fromVC.nameLabel
+        }
+        
+        
+        return animator
+    }
+    
     
     
 //    method, create and configure an Animator object, and return it. The animator will need to have its source and destination imageView and label properties set. Note: You must keep a strong reference to the animator object in your navigation controller delegate. Use a property to hold it instead of creating a temporary instance and referencing it solely in this method.
